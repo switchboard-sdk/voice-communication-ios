5fb49ff19b706e0e3755848f1c4d2ca9b6714fed
Balázs Kiss <dev@balazskiss.name>
2026-02-05 14:12:39 +0800

    Update iOS Simulator destination to iPhone 17

--- OnlineRadioApp/scripts/test.sh
+++ OnlineRadioApp/scripts/test.sh
@@ -5,7 +5,7 @@ set -eu
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 XCODE_PROJECT_PATH="${PROJECT_DIR}/OnlineRadioApp/OnlineRadioApp.xcodeproj"
 SCHEME_NAME="OnlineRadioApp"
-DESTINATION="platform=iOS Simulator,name=iPhone 16"
+DESTINATION="platform=iOS Simulator,name=iPhone 17"
 
 xcodebuild clean test \
   -project "$XCODE_PROJECT_PATH" \
